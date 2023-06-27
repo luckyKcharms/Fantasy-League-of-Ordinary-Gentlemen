55447ef3084169682cbcc36d6649ad19156718a7
luckyKcharms <127047582+luckyKcharms@users.noreply.github.com>
2023-06-26 20:50:25 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -9,10 +9,6 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 export const homepageText = `
   <p>LEAGUE OVERVIEW</p>
   <p>Welcome to FLOG, the Fantasy League of Ordinary Gentlemen. This is a 12-team league that was formed in 2021 by a group of owners who live in Western Canada. This league is considered a money league, and all participants are expected to pay their league fee before the draft begins. This league is designed to be a competition, but not a cut-throat league where anything goes. The rules described below are designed to act as a guideline for overall league play, and any disputes will be handled by the executive committee of the league. Please remember the overall goal of this league is to have fun and enjoy the game of football. If you have any questions or concerns, please do not hesitate to ask.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 `;
 
 /*   STEP 3   */
